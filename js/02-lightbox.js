8d713696334dd313d2715bee3a07aa8d922f6ec0
Alina Mikchalchuk <alvikul1994@gmail.com>
2021-10-23 19:36:42 +0300

    добавляет разметку

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = createMarkup(galleryItems);
 
-console.log(galleryItems);
+function createMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+    }).join('');
+}
+
+
+console.log(gallery);
