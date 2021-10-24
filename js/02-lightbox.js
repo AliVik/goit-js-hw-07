1ed8096e0aa4820bbc8a3c9b4d44bf39767e8081
Alina Mikchalchuk <alvikul1994@gmail.com>
2021-10-24 16:18:16 +0300

    добавляет опции библиотеке

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,14 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
 gallery.innerHTML = createMarkup(galleryItems);
+gallery.addEventListener('click', onImgClick);
 
 function createMarkup(items) {
     return items.map(({ preview, original, description }) => {
-        return `<li><a class="gallery__item" href="${original}">
+        return `<li class="gallery__item"><a href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a></li>`
     }).join('');
 }
 
 
-console.log(gallery);
+function onImgClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+}
+
+
+new SimpleLightbox('.gallery a',
+    {
+        captionsData: "alt",
+        captionPosition: 'bottom',
+        captionDelay: '250',
+        focus: false,
+        
+    });
+
